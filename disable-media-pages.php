9e498dcfd3214759fa642cf2fa2e0669be710c15
Reda EL Fillali <reda.elfillali@gmail.com>
2023-06-22 16:21:07 +0100

    code optimisation

--- disable-media-pages.php
+++ disable-media-pages.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Disable Media Pages
 Description: Disable media attachment pages.
-Version: 1.0
+Version: 1.0.1
 Author: Reda El Fillali
 License: GPL2
 text-domain: disable-media-pages
@@ -10,4 +10,3 @@ text-domain: disable-media-pages
 
 require_once(plugin_dir_path(__FILE__) . 'includes/functions.php');
 require_once(plugin_dir_path(__FILE__) . 'admin/admin.php');
-
--- includes/functions.php
+++ includes/functions.php
@@ -1,8 +1,10 @@
 <?php
-// Function to disable media pages
-function disable_media_pages_redirect() {
-    global $post;
+if (!defined('ABSPATH')) {
+    exit;
+}
 
+// Disables access to media attachment pages
+function disable_media_pages_redirect() {
     if (is_attachment()) {
         wp_redirect(home_url());
         exit;
@@ -10,55 +12,35 @@ function disable_media_pages_redirect() {
 }
 add_action('template_redirect', 'disable_media_pages_redirect');
 
-// Function to get ignored media files
+// Retrieves ignored media files
 function disable_media_pages_get_ignored_files() {
-    // Retrieve ignored media files from saved options
-    $ignored_files = get_option('disable_media_pages_ignored_files', array());
-
-    return $ignored_files;
+    return (array) get_option('disable_media_pages_ignored_files', array());
 }
 
-// Function to check if a media file should be ignored
+// Checks if a media file should be ignored
 function disable_media_pages_should_ignore_file($file) {
     $ignored_files = disable_media_pages_get_ignored_files();
-
-    // Check if the file is in the ignored files list
-    if (in_array($file, $ignored_files)) {
-        return true;
-    }
-
-    return false;
+    return in_array($file, $ignored_files);
 }
 
-// Function to add a media file to the ignored files list
+// Adds a media file to the ignored files list
 function disable_media_pages_add_ignored_file($file) {
     $ignored_files = disable_media_pages_get_ignored_files();
-
-    // Add the file to the ignored files list
     $ignored_files[] = $file;
-
-    // Update the options
     update_option('disable_media_pages_ignored_files', $ignored_files);
 }
 
-// Function to remove a media file from the ignored files list
+// Removes a media file from the ignored files list
 function disable_media_pages_remove_ignored_file($file) {
     $ignored_files = disable_media_pages_get_ignored_files();
-
-    // Find and remove the file from the ignored files list
     $index = array_search($file, $ignored_files);
     if ($index !== false) {
         unset($ignored_files[$index]);
-
-        // Reindex the array keys
-        $ignored_files = array_values($ignored_files);
-
-        // Update the options
-        update_option('disable_media_pages_ignored_files', $ignored_files);
+        update_option('disable_media_pages_ignored_files', array_values($ignored_files));
     }
 }
 
-// Function to load plugin text domain for translation
+// Loads plugin text domain for translation
 function disable_media_pages_load_textdomain() {
     load_plugin_textdomain('disable-media-pages', false, dirname(plugin_basename(__FILE__)) . '/languages/');
 }
